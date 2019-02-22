676a5e54e81f0e32f1d9b1eb77de699430d7b487
Sampo Savolainen <sampo.savolainen@spatineo.com>
2019-02-22 16:43:02 +0200

    Updates

---
+++ precache-manifest.c93dd15651e1df95129dc3620c61518f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3ede19d9589a05d8f8",
+    "url": "/sofp-demo/static/js/runtime~main.2c3ede19.js"
+  },
+  {
+    "revision": "97f5302ac4023d4ec9df",
+    "url": "/sofp-demo/static/js/main.97f5302a.chunk.js"
+  },
+  {
+    "revision": "2bd70dacce1f8f4af0a0",
+    "url": "/sofp-demo/static/js/1.2bd70dac.chunk.js"
+  },
+  {
+    "revision": "97f5302ac4023d4ec9df",
+    "url": "/sofp-demo/static/css/main.7bc6b690.chunk.css"
+  },
+  {
+    "revision": "2bd70dacce1f8f4af0a0",
+    "url": "/sofp-demo/static/css/1.b8d1cadd.chunk.css"
+  },
+  {
+    "revision": "9065a69a4422218219bf26f107080e6a",
+    "url": "/sofp-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/sofp-demo/precache-manifest.58b9de44263961f329722f400e1dfc6a.js"
+  "/sofp-demo/precache-manifest.c93dd15651e1df95129dc3620c61518f.js"
 );
 
 workbox.clientsClaim();
